224dfc498d38944bdd9ad7b297d29e3031e6ce00
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-09 12:16:31 -0400

    added more code

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -14,11 +14,20 @@ public class ArrayUtility<SomeType> {
 
     public SomeType findOddOccurringValue() {
 
-        return getNumberOfOccurrences();
+        return null;
     }
 
     public SomeType findEvenOccurringValue() {
+        int lowest = 0;
+        int number = 0;
+        for(int i = 0; i < array.length; i++){
+            if(getNumberOfOccurrences(array[i]) > lowest){
+                lowest = getNumberOfOccurrences(array[i]);
+                number = (Integer) array[i];
+            }
+        }
         return null;
+
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
